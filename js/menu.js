03259f560b45974ea7893199f4f057c8c7970020
debbichinaim <74429658+debbichinaim@users.noreply.github.com>
2020-11-25 16:22:15 +0100

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+<script>
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
+</script>
